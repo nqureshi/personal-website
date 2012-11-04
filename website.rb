f40393f2ad47b0e90460b0387289f1b5f3021560
Nabeel Qureshi <nabeelqureshi67@gmail.com>
2012-11-04 15:00:20 +0000

    added poem generator

--- website.rb
+++ website.rb
@@ -1,8 +1,12 @@
 require 'rubygems'
 require 'sinatra/base'
 require 'haml'
+require 'nokogiri'
+require 'open-uri'
 
 class Website < Sinatra::Base
+  
+  POEMS_URL = "http://rpo.library.utoronto.ca/poems"
 
   get '/' do
     haml :index
@@ -16,4 +20,18 @@ class Website < Sinatra::Base
     haml :quotes
   end
 
+  get '/poem' do
+    @poem = Nokogiri::HTML(open(get_random_poem))
+    @poem_title = @poem.css("#page-title")
+    @poem_author = @poem.css(".poet-name-in-poem")
+    @poem_text = @poem.css(".line-text")
+    haml :poem, :layout => :poemlayout
+  end
+  
+  helpers do
+    def get_random_poem # Gets a random poem from the RPO library of poems
+      poem_url = Nokogiri::HTML(open(POEMS_URL)).css("#block-system-main a")[rand(1..4785)]["href"]
+      POEMS_URL.gsub("/poems", "") + poem_url
+    end
+  end
 end
