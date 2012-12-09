086262cea83a31ed48556d18a2991bba9431bd7a
Nabeel Qureshi <nabeelqureshi67@gmail.com>
2012-12-09 15:42:43 +0000

    as before

---
+++ lib/blog/post.rb
@@ -0,0 +1,31 @@
+module Nabeel
+  class Post
+    
+    ROUTE = '/blog/:slug/?'
+    
+    def initialize(config)
+      @config = config
+    end
+    
+    def title
+      @config['title'].split('-').each{|word| word.capitalize!}.join(' ')
+    end
+    
+    def content
+       Kramdown::Document.new(@config['content']).to_html
+    end
+    
+    def slug
+      @slug || @config['slug'] || title.downcase.strip.gsub(/\s/, '-').gsub(/[^a-z0-9-]/, '')
+    end
+    
+    def path
+      "/posts/#{ slug }"
+    end
+    
+    def date
+      @config['date']
+    end
+    
+  end
+end
